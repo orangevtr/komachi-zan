ae75e041de04dfa7b15853fa1a5cd3528b3a171d
orangevtr <orangevtr@users.noreply.github.com>
2017-01-24 16:29:22 +0900

    rewrite with arrow func

--- komachizan.js
+++ komachizan.js
@@ -1,25 +1,24 @@
-find_komachi(process.argv.slice(2));
-
-function find_komachi(values) {
+((values) => {
   var opes = ['+','-','*','/'];
-  function add_ope(ar,v) {
-    var res = [];
-    ar.forEach(function(e,i) {
-      opes.forEach(function(ope,opei) {
-	    res.push(e + ope + v);
-	  });
-    });
-    return res;
-  };
-  
-  values.forEach(function(val,id) {
-	if (id > 0) {
-      formulas = add_ope(formulas,val);
-    } else {
-      formulas = [val];
-    }
+  values.forEach((val,id) => {
+      if (id > 0) {
+          formulas = ((ar, v) => {
+              var res = [];
+              ar.forEach((e,i) => {
+                  opes.forEach((ope,opei) => {
+                      res.push(e + ope + v);
+                  });
+              });
+              return res;
+          })(formulas, val);
+      } else {
+          formulas = [val];
+      }
   });
-  formulas.filter(function(f) {return eval(f) == 100}).forEach(function(f,id) {
-    console.log("[" + id + "]: " + f);
+
+  formulas.filter((f) => {
+      return eval(f) == 100;
+  }).forEach((f,id) => {
+      console.log("[" + id + "]: " + f);
   });
-}
+})(process.argv.slice(2));
